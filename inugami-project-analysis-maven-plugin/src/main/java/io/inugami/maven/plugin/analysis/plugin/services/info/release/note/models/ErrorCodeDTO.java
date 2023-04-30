443e2bbe029cc8a7e7bad9538af250044b656b1b
Patrick Guillerm <pguillerm@smartwavesa.com>
2023-04-30 13:46:18 +0200

    #67 FEAT : add all error code information in release note

--- inugami-project-analysis-maven-plugin/src/main/java/io/inugami/maven/plugin/analysis/plugin/services/info/release/note/models/ErrorCodeDTO.java
+++ inugami-project-analysis-maven-plugin/src/main/java/io/inugami/maven/plugin/analysis/plugin/services/info/release/note/models/ErrorCodeDTO.java
@@ -30,19 +30,29 @@ import static io.inugami.maven.plugin.analysis.api.utils.Constants.UNDERSCORE;
 public class ErrorCodeDTO implements Comparable<ErrorCodeDTO>, JsonObject {
     private static final long serialVersionUID = -5469719611823975510L;
     @EqualsAndHashCode.Include
-    String  errorCode;
+    String errorCode;
     Integer statusCode;
     String  message;
+    String  messageDetail;
+    String  payload;
     String  type;
 
-    String  artifact;
+    Boolean exploitationError;
+    Boolean rollback;
+    Boolean retryable;
+
+    String field;
+    String url;
+    String errorDomain;
+    String errorSubDomain;
+    String artifact;
 
     @Override
     public int compareTo(final ErrorCodeDTO other) {
-        final String currentHash = String.join(UNDERSCORE,String.valueOf(artifact),String.valueOf(errorCode));
+        final String currentHash = String.join(UNDERSCORE, String.valueOf(artifact), String.valueOf(errorCode));
         final String otherHash = String.join(UNDERSCORE,
-                                             String.valueOf(other==null?null:other.getArtifact()),
-                                             String.valueOf(other==null?null:other.getErrorCode()));
+                                             String.valueOf(other == null ? null : other.getArtifact()),
+                                             String.valueOf(other == null ? null : other.getErrorCode()));
         return currentHash.compareTo(otherHash);
     }
 }
