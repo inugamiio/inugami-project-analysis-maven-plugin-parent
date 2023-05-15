54e8d1a006d4cd08d5f750b59250b473a545174c
Patrick Guillerm <pguillerm@smartwavesa.com>
2023-05-15 21:16:32 +0200

    #83 FIX : manage spring context path

--- inugami-project-analysis-front/inugami-project-analysis-front-springboot/src/main/java/io/inugami/maven/plugin/analysis/front/springboot/configuration/SkipIologOnRelease.java
+++ inugami-project-analysis-front/inugami-project-analysis-front-springboot/src/main/java/io/inugami/maven/plugin/analysis/front/springboot/configuration/SkipIologOnRelease.java
@@ -1,11 +1,33 @@
 package io.inugami.maven.plugin.analysis.front.springboot.configuration;
 
 import io.inugami.monitoring.api.resolvers.Interceptable;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class SkipIologOnRelease implements Interceptable {
+
+    public static final String SEP              = "/";
+    public static final String RELEASE_NOTE_APP = "release-note-app";
+    private final       String currentContextPath;
+    private final       String releaseNotePath;
 
     @Override
     public boolean isInterceptable(final String uri) {
-        return !uri.contains(MvcConfiguration.CURRENT_PATH.get());
+        if (releaseNotePath == null) {
+            return !uri.contains(RELEASE_NOTE_APP);
+        } else {
+            String fullPath = uri;
+
+            if (currentContextPath != null) {
+                final StringBuilder builder = new StringBuilder(currentContextPath);
+                if (!uri.startsWith(SEP)) {
+                    builder.append(SEP);
+                }
+                builder.append(uri);
+                fullPath = builder.toString();
+            }
+            return !(fullPath.contains(releaseNotePath) || (fullPath + SEP).contains(releaseNotePath));
+        }
+
     }
 }
